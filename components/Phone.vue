8c99ef3058eb6c6152fdceff72427f2d19733d50
Martin Campagnoli <martin.campagnoli@incentro.com>
2022-09-26 16:52:03 +0200

    Add first design to phone component

--- components/Phone.vue
+++ components/Phone.vue
@@ -1,8 +1,24 @@
 /* eslint-disable vue/require-prop-types */
 <template>
   <div>
-    Here render one phone
-    {{ phone.name }}
+    <div class="card" style="width:350px">
+      <img class="card-img-top" :src="`https://source.unsplash.com/random/300x100?sig=${phone.id}`" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">
+          {{ phone.name }}
+        </h5>
+        <p class="card-text">
+          {{ phone.model }}
+          <br>
+          <br>
+          <span>has_5g: {{ phone.has_5g ? 'yes' : 'no' }}</span><br>
+          <span>has_esim: {{ phone.has_esim ? 'yes' : 'no' }}</span><br>
+          <span>has_promotion: {{ phone.has_promotion ? 'yes' : 'no' }}</span><br>
+          <span>has_usp: {{ phone.has_usp ? 'yes' : 'no' }}</span><br>
+        </p>
+          <small class="text-muted">Last updated 3 mins ago</small>
+      </div>
+    </div>
   </div>
 </template>
 
