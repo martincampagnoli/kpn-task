3fc530b2402172c7c7789c9f2e259e6921559ffe
Martin Campagnoli <martin.campagnoli@incentro.com>
2022-09-26 16:52:42 +0200

    Add basic filter functionality

--- components/Phone-list.vue
+++ components/Phone-list.vue
@@ -1,8 +1,41 @@
 <template>
   <div>
-    <span>Here render phone list</span>
-    <div v-for="item in phones" :key="item.id">
-      <phone :phone="item" />
+    <div class="container">
+      <div class="row">
+        <div class="col-4">
+          <span>Has 5g?</span>
+          <select v-model="has_5g" class="form-control" @change="applyFilter()">
+            <option value="">
+              All
+            </option>
+            <option value="true">
+              Yes
+            </option>
+            <option value="false">
+              No
+            </option>
+          </select>
+        </div>
+        <div class="col-4">
+          <span>Filter by Name</span>
+          <input v-model="name" type="text" placeholder="Enter name here" @keyup="applyFilter()">
+        </div>
+        <div class="col-4">
+          <span>Filter by Color</span>
+          <input v-model="color" type="text" placeholder="Enter color here" @keyup="applyFilter()">
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="card-deck">
+          <div v-for="item in filteredPhones" :key="item.id">
+            <phone :phone="item" />
+            <br>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -15,7 +48,11 @@ export default {
   components: { Phone },
   data () {
     return {
-      phones: {}
+      phones: {},
+      filteredPhones: {},
+      name: '',
+      has_5g: '',
+      color: ''
     }
   },
   mounted () {
@@ -26,8 +63,28 @@ export default {
       axios.get('/phone_feed.json')
         .then((response) => {
           this.phones = response.data.products
+          this.filteredPhones = this.phones
         })
+    },
+    applyFilter () {
+      console.dir(this.filteredPhones)
+      this.filteredPhones = this.filterPhonesBy5g(this.filterPhonesByName(this.filterPhonesByColor(this.phones)))
+    },
+    filterPhonesBy5g (phones) {
+      if (!this.has_5g) { return phones }
+      return phones.filter(e => e.has_5g === JSON.parse(this.has_5g))
+    },
+
+    filterPhonesByName (phones) {
+      if (!this.name) { return phones }
+      return phones.filter(e => e.name.toLowerCase().includes(this.name))
+    },
+
+    filterPhonesByColor (phones) {
+      if (!this.color) { return phones }
+      return phones.filter(e => e.colors.includes(this.color))
     }
+
   }
 
 }
